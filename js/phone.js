9a9de33dbdef71b4ae2ffd37183369a26920e6a0
Mehedi Hasan <iammeheditanjil@gmail.com>
2025-05-15 02:11:27 +0600

    html and js added

---
+++ js/phone.js
@@ -0,0 +1,16 @@
+const loadPhone = async() =>{
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const data = await res.json();
+    const phones = data.data;
+    // console.log(phones);
+    displayPhones(phones);
+}
+
+
+const displayPhones = phones => {
+    console.log(phones);
+}
+
+loadPhone();
+
+
