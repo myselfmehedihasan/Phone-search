79e3c5e09cd01dd4db70fe922690411c292b5d54
Mehedi Hasan <iammeheditanjil@gmail.com>
2025-05-15 06:01:00 +0600

    reload button added

--- js/phone.js
+++ js/phone.js
@@ -1,4 +1,4 @@
-const loadPhone = async (searchText = 'iphone', isShowAll) => {
+const loadPhone = async (searchText = '13', isShowAll) => {
 
      
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
